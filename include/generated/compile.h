13b6c768f75eec4af636260d593951f19435f77e
xuejin <xuejin@hct.hk>
2019-07-30 14:15:22 +0800

    add drv

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#1 SMP Mon Apr 9 20:07:30 CST 2018"
+#define LINUX_COMPILE_BY "mtk15778"
+#define LINUX_COMPILE_HOST "mtkslt208"
+#define LINUX_COMPILER "gcc version 4.6.3 (Buildroot 2012.11.1) "
